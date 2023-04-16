5b4cbe2a112276ec3e676b5f83ebb0cd9f51e068
Antonio Merlos <monty.merlos@gmail.com>
2023-04-16 14:21:13 +0200

    Initial commit

---
+++ src/test/java/com/merlos/antonio/superheroapi/SuperheroApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.merlos.antonio.superheroapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperheroApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
