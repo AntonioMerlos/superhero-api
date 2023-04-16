7c593b57db9bc53c0aafe8b27a9a0f221f745452
Antonio Merlos <monty.merlos@gmail.com>
2023-04-16 18:05:16 +0200

    Create repository

---
+++ src/main/java/com/merlos/antonio/superheroapi/repository/SuperheroRepository.java
@@ -0,0 +1,13 @@
+package com.merlos.antonio.superheroapi.repository;
+
+import com.merlos.antonio.superheroapi.model.Superhero;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface SuperheroRepository extends JpaRepository<Superhero, Long> {
+
+    List<Superhero> findByNameContaining(String value);
+}
