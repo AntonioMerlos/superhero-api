697155dc4c675edea41b40b86f0f2b6db7a4d1cc
Antonio Merlos <monty.merlos@gmail.com>
2023-04-16 16:06:56 +0200

    Create superhero entity

---
+++ src/main/java/com/merlos/antonio/superheroapi/model/Superhero.java
@@ -0,0 +1,21 @@
+package com.merlos.antonio.superheroapi.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Superhero {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+}
